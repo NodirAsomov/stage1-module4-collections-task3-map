d021406103afe18d9ae026d077671b60bbb4880b
Нодир Асомов <nasomov93@gmail.com>
2024-06-15 08:22:25 +0500

    map

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,23 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> result = new HashMap<>();
+
+        if (sourceMap.size() > 0) {
+            for (Integer number : sourceMap.keySet()) {
+                String word = sourceMap.get(number);
+
+                if ((result.containsKey(word)) && (number > result.get(word))) {
+                    continue;
+                }
+
+                result.put(word, number);
+            }
+        }
+
+        return result;
     }
 }
